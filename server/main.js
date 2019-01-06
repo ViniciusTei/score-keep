e04c6757825db9adb4d693e5e95571dffa83276f
ViniciusTei <41354297+ViniciusTei@users.noreply.github.com>
2019-01-06 18:34:12 -0200

    initial commit

---
+++ server/main.js
@@ -0,0 +1,22 @@
+import { Meteor } from 'meteor/meteor';
+import { Players } from './../imports/api/players';
+
+Meteor.startup(() => {
+
+});
+
+// let house = {
+//   bedrooms: 2,
+//   bathrooms: 1.5
+// };
+//
+// let yB = 1995;
+//
+// let obj = {
+//   ...house,
+//   bedrooms: 3,
+//   yB,
+//   flooring: 'Carpet'
+// };
+//
+// console.log(obj);
